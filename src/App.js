629ba774810bd5e8604ffe8eb682a7c4f1e3a326
athousandleaves <superamare@gmail.com>
2018-01-21 21:15:37 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Nicholas Keeling</h1>
+        <h2>Full Stack JavaScript Developer</h2>
+        <p>Hi! I'm looking for a job. Can you help me?</p>
+      </div>
+    );
+  }
+}
+
+export default App;
