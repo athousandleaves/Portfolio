e278ba0de3bd1f787954f63b5787e5bbaed6ff7a
athousandleaves <superamare@gmail.com>
2018-01-24 08:08:42 -0600

    add thumbnail images to projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,28 +8,28 @@ export default class Projects extends Component {
         description: "A coding tutorial library that utilizes Mongoose and the YouTube API",
         link: "https://limitless-springs-46654.herokuapp.com/",
         github: "https://github.com/athousandleaves/Qtorial",
-        thumbnail: "img src"
+        thumbnail: "https://i.imgur.com/qOIpjdu.png"
       },
       {
         name: "Pixelizer",
         description: "A pixel art generator utilizing CSS Grid",
         link: "http://pixelizer.surge.sh/",
         github: "https://github.com/athousandleaves/Pixelizer", 
-        thumbnail: "img src"
+        thumbnail: "https://i.imgur.com/c4y0KAn.png"
       },
       {
         name: "Listener",
         description: "A music search app that utilizes the Last.fm API to find and display artists' top songs. Each search generates YouTube links for each song and aggregates the links into a playlist.",
         link: "http://listener.surge.sh/",
         github: "https://github.com/athousandleaves/Listener",
-        thumbnail: "img src"
+        thumbnail: "https://i.imgur.com/tmOmYUA.png"
       },
       {
         name: "Itemizer",
         description: "A list-making app that utilizes the Web Speech API for asynchronous speech recognition",
         link: "http://itemizer.surge.sh/",
         github: "https://github.com/athousandleaves/Itemizer",
-        thumbnail: "img src"
+        thumbnail: "https://i.imgur.com/qThfMJU.png"
       }
     ];
     
@@ -38,6 +38,9 @@ export default class Projects extends Component {
         <a href={project.link} className="projectName"><h2>{project.name}</h2></a>
         <a href={project.github} className="githubLink">{project.github}</a>
         <p className="projectDescription">{project.description}</p>
+        <div className="projectThumb">
+          <img src={project.thumbnail} alt={project.name} className="projectPic"/>
+        </div>
       </div>
     );
 
